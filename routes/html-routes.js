9ba85fb9f08941a8c7bf4778f449b8b01dad4cd0
SandraR <srodrig284@gmail.com>
2017-05-22 19:31:15 -0400

    New files

---
+++ routes/html-routes.js
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    // index route loads view.html
+    app.get("/", function(req, res) {
+        // GET route for main page
+       // res.render('index.html', { message: req.flash('loginMessage') });
+        res.sendFile(path.join(__dirname, "../index.html"));
+    });
+
+};
--- server.js
+++ server.js
@@ -4,34 +4,29 @@ var bodyParser = require("body-parser");
 
 //  *******   ADD OTHER DEPENDENCIES HERE   **************
 
-// Requires models js files
-var db = require("./models");
 // Set up for the Express App
 var app = express();
 
 // Needed for Heroku deployment
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+// Requires models js files
+var db = require("./models");
 
 //  { extended: true } allows for qs library use for parsing (instead of querystring)
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Serve static content for the app from the "public" directory in the 
 //  application directory.   process.cwd() used for sequelize
-app.use(express.static(process.cwd() + "/public"));
-
-// Set Handlebars
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use(express.static("./public"));
 
 // Need to add the other api-routes here
-// require("./routes/api-routes.js")(app);
+//require("./routes/user-api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
 	app.listen(PORT, function() {
 	  console.log("App listening on PORT " + PORT);
 	});
