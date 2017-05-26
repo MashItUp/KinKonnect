dbcf55945f9c258e6aeeac7419f44ce5d618a952
SandraR <srodrig284@gmail.com>
2017-05-26 15:35:43 -0400

    fixing api

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -7,27 +7,19 @@
 
 // Requiring our models
 var db = require("../models");
+var path = require("path");
 
 // Routes
 // =============================================================
 module.exports = function(app, passport) {
-
-    // GET route for getting all of the users
-    /*   app.get("/api/allpersons", function(req, res) {
-           // findAll returns all entries for a table when used with no options
-           db.Person.findAll({}).then(function(dbPerson) {
-               // We have access to the todos as an argument inside of the callback function
-               res.json(dbPerson);
-           });
-       });*/
 
     // =====================================
     // LOGIN ===============================
     // =====================================
     // process the login form
     app.post('/api/user/login', passport.authenticate('local-login', {
-        successRedirect: '/index.html', // redirect to the secure profile section
-        failureRedirect: '/index.html', // redirect back to the signup page if there is an error
+        successRedirect: '/dashboard', // redirect to the secure profile section
+        failureRedirect: '/index', // redirect back to the signup page if there is an error
         failureFlash: 'Invalid username or password.' // allow flash messages
     }));
 
@@ -36,8 +28,8 @@ module.exports = function(app, passport) {
     // SIGNUP ==============================
     // =====================================
     app.post('/api/user/signup', passport.authenticate('local-signup', {
-        successRedirect: '../index.html', // redirect to the secure profile section
-        failureRedirect: '../index.html', // redirect back to the signup page if there is an error
+        successRedirect: '/dashboard', // redirect to the secure profile section
+        failureRedirect: '/index', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
 
@@ -47,14 +39,21 @@ module.exports = function(app, passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    /*   app.get('/dashboard', isLoggedIn, function(req,res){
-           console.log('dashboard');
-           console.log('req.req.use = ', req.user);
-           console.log('res.json = ', res.json());
-           res.render('dashboard.html', {
-               user : req.user // get the user out of session and pass to template
-           });
-       })*/
+       app.get('/dashboard', isLoggedIn, function(req,res){
+           console.log('got to dashboard');
+           //console.log('req.user = ', req.user);
+           /*res.render('../dashboard', {
+               userid : req.user.id, // get the user out of session and pass to template
+               fname: req.user.first_name,
+               lname: req.user.last_name
+           });*/
+           res.sendFile(path.join(__dirname, "../public/index.html"));
+       });
+
+    app.get('/index', function(req, res) {
+        res.redirect('/');
+    });
+
 
     // =====================================
     // LOGOUT ==============================
